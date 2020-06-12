b396b769ad553bc1a1cbd5c499d756be59908f3d
jonMa99 <jonathanmah99@gmail.com>
2020-06-11 17:51:08 -0700

    Added php version of pages

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+
+    <body>
+        <h1>Login Page</h1>
+        <a href="main.php">Go back</a>
+        <br/>
+        <a href="main.php">Make an account</a>
+    </body>
+</html>
