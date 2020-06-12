f64e7ae9c77427e9a4c4c246e100bd112dfa64e6
jonMa99 <jonathanmah99@gmail.com>
2020-06-11 22:30:08 -0700

    Added the makeaccount page

---
+++ src/makeaccount.php
@@ -0,0 +1,10 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$name = $_POST['name'];
+$phonenumber = $_POST['phonenumber'];
+echo "UN is $username <br/>";
+echo "PW is $password <br/>";
+echo "N is $name <br/>";
+echo "PH is $phonenumber <br/>";
+?>
