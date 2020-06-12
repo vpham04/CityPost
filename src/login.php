db393aae84d069f6392f7c08b37cfbc517641b3f
vpham04 <bvpham7@gmail.com>
2020-06-11 23:04:58 -0700

    Updated pictures

--- src/login.php
+++ src/login.php
@@ -5,9 +5,14 @@
     </head>
 
     <body>
+        <h1>Contact information</h1>
+        <p id="contact">Email:city@post.ca | Phone: (123) 456-7890
+             <br> Address: 123 Main Street Vancouver, BC, Canada </p>
+        <br/>
         <h1>Login Page</h1>
         <a href="/src/main.php">Go back</a>
         <br/>
         <a href="/src/main.php">Make an account</a>
+
     </body>
 </html>
--- src/main.php
+++ src/main.php
@@ -6,13 +6,9 @@
     </head>
 
     <body>
-        <h1>City Post</h1>
-
-        <p id="contact">Email:city@post.ca | Phone: (123) 456-7890
-             <br> Address: 123 Main Street Vancouver, BC, Canada </p>
-
+        <h1>CITY POST
         <p id="slogan">We get your packages to you!</p>
-
+        </h1>
         <a href="/src/login.php" style='margin:0 auto; display: block; width: 60%;'>
             <img id="package" class="image" src="../resources/Package.svg">
         </a>
