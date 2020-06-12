0adaa86a143cfa7c3e6e9ae6eca978644cd06837
Jordan Dang <jordandang1@hotmail.com>
2020-06-11 19:28:49 -0700

    seperated stylesheet into a css file

--- src/main.php
+++ src/main.php
@@ -1,64 +1,8 @@
 <!DOCTYPE html>
 <html>
-    <style>
-        /* Makes it wrap all the way to the end*/
-        html, body {
-            margin: 0;
-            padding: 0;
-        }
-
-        h1 {
-            text-align: center;
-            color: navy;
-            background-color: grey;
-            font-size: 35px;
-            padding: 40px 40px 60px 40px;
-            margin: 0px;
-        }
-
-        body {
-            background-color:#dedede;
-        }
-
-        #contact {
-            font-size: 20px;
-            text-align: center;
-            margin: -55px;
-        }
-
-        #github {
-            font-size: 20px;
-        }
-
-        p {
-            margin: 60px 30px 30px 60px;
-        }
-
-        #slogan {
-            margin-top: 125px;
-            font-size: 40px;
-            text-align: center;
-        }
-    
-
-        .image {
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-        #package {
-            width: 90%;
-            height: 90%;
-        }
-
-        body {
-            overflow-x: hidden;
-        }
-
-    </style>
-
     <head>
         <title>City Post</title>
+        <link rel="stylesheet" href="styles.css">
     </head>
 
     <body>
