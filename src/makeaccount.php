3023f0daa4990ed9b37b9f2607ad7a9bf8caef48
jonMa99 <jonathanmah99@gmail.com>
2020-06-12 00:48:54 -0700

    Made makeaccount enter info into database but throwing error still

--- src/makeaccount.php
+++ src/makeaccount.php
@@ -1,10 +1,23 @@
 <?php
+include 'connect.php';
+$conn = OpenCon();
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 $name = $_POST['name'];
 $phonenumber = $_POST['phonenumber'];
-echo "UN is $username <br/>";
-echo "PW is $password <br/>";
-echo "N is $name <br/>";
-echo "PH is $phonenumber <br/>";
-?>
+
+
+$sql = "INSERT INTO Accounts(Username, Password, LvlAccess, CID, SSN)
+VALUES ('$username', '$password', 1, NULL, NULL)";
+$result = $conn->query($sql);
+
+if ($conn->query($sql) == TRUE) {
+    echo "New record created successfully";
+} else {
+    /* This line says duplicate PK inserted even on first insert
+    but still inserts it while spitting out the error
+    */
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+CloseCon($conn);
