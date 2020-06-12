f7fdbd60e4c886d9dc176e670fe5fabe78ee9185
jonMa99 <jonathanmah99@gmail.com>
2020-06-12 14:41:29 -0700

    Fixed failed to make account bug

--- src/makeaccount.php
+++ src/makeaccount.php
@@ -12,12 +12,9 @@ $sql = "INSERT INTO Accounts(Username, Password, LvlAccess, CID, SSN)
 VALUES ('$username', '$password', 1, NULL, NULL)";
 $result = $conn->query($sql);
 
-if ($conn->query($sql) == TRUE) {
-    echo "New record created successfully";
+if ($result == TRUE) {
+    echo "New account created successfully";
 } else {
-    /* This line says duplicate PK inserted even on first insert
-    but still inserts it while spitting out the error
-    */
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Please choose another password";
 }
 CloseCon($conn);
