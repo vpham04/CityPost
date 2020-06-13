e64f1fa3f571556ca606efb2e2da7bdf13f9b1bf
jonMa99 <jonathanmah99@gmail.com>
2020-06-12 19:28:38 -0700

    Added transaction to makeaccount so failing to make account doesnt add unnecessary data

--- src/makeaccount.php
+++ src/makeaccount.php
@@ -7,6 +7,7 @@ $password = $_POST['password'];
 $name = $_POST['name'];
 $phonenumber = $_POST['phonenumber'];
 
+$conn->begin_transaction();
 
 $sql = "INSERT INTO Accounts(Username, Password, LvlAccess, CID, SSN)
 VALUES ('$username', '$password', 1, NULL, NULL)";
@@ -24,13 +25,15 @@ $result1 = $conn->query($sql1);
 
 if ($result == true & $result1 == true) {
     echo "New account created successfully";
+    $conn->commit();
 } else if ($result == false & $result1 == false) {
     echo "Failed making an account";
-}
-// else if ($result1 == false) {
-//     echo "CID already exist";
-// }
-else {
+    $conn->rollback();
+} else if ($result1 == false) {
+    echo "CID already exist";
+    $conn->rollback();
+} else {
     echo "Please choose another username";
+    $conn->rollback();
 }
 CloseCon($conn);
