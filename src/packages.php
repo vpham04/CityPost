7edbfd23107129d4f66baf4ce7dc6e143648ebef
vpham04 <bvpham7@gmail.com>
2020-06-15 16:31:12 -0700

    added packages, routes pages, updated courier to have buttons to those pages

---
+++ src/packages.php
@@ -0,0 +1,46 @@
+<?php
+session_start()
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Package</title>
+    <link rel="stylesheet" href="styles.css">
+    <style>
+        .border-class {
+            width: 35%;
+            font-size: 25px;
+            border-bottom-style: solid;
+            border-bottom-color: black;
+            border-bottom-width: 3px;
+        }
+
+        #title {
+            text-align: left;
+        }
+
+        .header {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        <h1>Package</h1>
+        <div class="account">
+            <button onclick="window.location.href='<?php echo $_SESSION['returnpage']; ?>'" class="account-button">Go back</button>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<?php
+
+include 'connect.php';
+$conn = OpenCon();
+
+CloseCon($conn);
