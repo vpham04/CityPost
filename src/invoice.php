3e2444bb24486c097ca395af30be074a35675c68
jonMa99 <jonathanmah99@gmail.com>
2020-06-15 18:44:47 -0700

    Changed invoice to by aggregate with group by

--- src/invoice.php
+++ src/invoice.php
@@ -3,32 +3,24 @@
 include 'connect.php';
 $conn = OpenCon();
 
-$sql = "SELECT IID, CID, Cost FROM Invoice";
+$sql = "SELECT CID, SUM(Cost) AS TotalSpent FROM Invoice GROUP BY CID";
 $result = $conn->query($sql);
-
-$sum = "SELECT SUM(Cost) AS Profit FROM Invoice";
-$result1 = $conn->query($sum);
 
 if ($result->num_rows > 0) {
     echo "<table>
         <tr>
-            <th id='title' class='border-class'>IID</th>
             <th id='title' class='border-class'>CID</th>
             <th id='title' class='border-class'>Cost</th>
         </tr>";
     while ($row = $result->fetch_assoc()) {
         echo
-            "<tr>
-            <td class='border-class'>" . $row["IID"] . "</td>
+        "<tr>
             <td class='border-class'>" . $row["CID"] . "</td>
-            <td class='border-class'>" . $row["Cost"] . "</td>
+            <td class='border-class'>" . $row["TotalSpent"] . "</td>
         </tr>";
     }
     echo "</table>";
-    $invoicesum = $result1->fetch_assoc();
-    echo "Profit sum: " . $invoicesum['Profit'];
 } else {
     echo "0 results";
 }
 CloseCon($conn);
-?>
