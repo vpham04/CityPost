514ab567f956d6adfd3973f416b4ec46072184b5
vpham04 <bvpham7@gmail.com>
2020-06-16 22:46:59 -0700

    added orders in courier

--- src/courier.php
+++ src/courier.php
@@ -67,7 +67,7 @@ session_start();
 
 
         <!-- TODO -->
-        <button onclick="window.location.href='../src/packages.php'" class="customer-option-button">View Packages</button>
+        <button onclick="window.location.href='../src/courierOrders.php'" class="customer-option-button">View Current Orders</button>
 
         <button onclick="window.location.href='../src/countorder.php'" class="customer-option-button">Number of Orders</button>
     </div>
